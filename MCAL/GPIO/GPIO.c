6266d29b84502f992fdb0068089cf1cbce9755ca
Eman <eman.mo29100@gmail.com>
2024-02-16 22:29:33 +0200

    GPIO in progress

--- MCAL/GPIO/GPIO.c
+++ MCAL/GPIO/GPIO.c
@@ -4,3 +4,120 @@
  * Created: 2/15/2024 9:00:35 PM
  *  Author: eman
  */ 
+#include "GPIO.h"
+/*NULL POINTER*/
+#define	NULL	                                (void*)0
+
+#define GPIO_MODER_MASK                         0x00000003 /* 2 BITS */
+#define GPIO_OTYPER_MASK                        0x00000001 /* 1 BIT */
+#define GPIO_OSPEEDR_MASK                       0x00000003
+#define GPIO_PUPDR_MASK                         0x00000003
+
+#define PIN_MODER_OFFSET                        0x00000002
+#define PIN_OTYPER_OFFSET                       0x00000001
+#define PIN_OSPEEDR_OFFSET                      0x00000002
+#define PIN_PUPDR_OFFSET                        0x00000002
+#define PIN_ALTERNATIVE_OFFSET                  0x00000004
+
+volatile GPIO_Registers_t* const GPIO_Ports[3] =
+{
+    (GPIO_Registers_t *)GPIOA,
+    (GPIO_Registers_t *)GPIOB,
+    (GPIO_Registers_t *)GPIOC
+};
+
+ErrorStatus_t GPIO_Init(GPIO_PinConfiguration_t* Add_pstrPinConfg){
+	ErrorStatus_t Loc_ErrorStatus = GPIO_Ok;
+	if(Add_pstrPinConfg == NULL){
+		Loc_ErrorStatus = GPIO_NULL;
+	}
+	else if(Add_pstrPinConfg->GPIO_Pin > GPIO_PIN_15){
+		Loc_ErrorStatus = GPIO_WrongPin;
+	}
+	else if(Add_pstrPinConfg->GPIO_Port > (u32)GPIOC){
+		Loc_ErrorStatus = GPIO_WrongPort;
+	}
+	else if(Add_pstrPinConfg->GPIO_ALTF > GPIO_AF15){
+		Loc_ErrorStatus = GPIO_WrongAltf;
+	}
+	else if(Add_pstrPinConfg->GPIO_Speed > GPIO_SPEED_VERYHIGH){
+		Loc_ErrorStatus = GPIO_WrongSpeed;
+	}
+	else if(Add_pstrPinConfg->GPIO_Mode > GPIO_MODE_AF_OD_PD){
+			Loc_ErrorStatus = GPIO_WrongMode;
+	}
+	else{
+		/* These bits are written by software to configure the I/O direction mode */
+	    u32 Loc_Temp = GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_MODER;
+	    Loc_Temp &= ~(GPIO_MODER_MASK << (Add_pstrPinConfg->GPIO_Pin*PIN_MODER_OFFSET));
+	    Loc_Temp |= ((Add_pstrPinConfg->GPIO_Mode & GPIO_MODER_MASK) << (Add_pstrPinConfg->GPIO_Pin*PIN_MODER_OFFSET));
+	    GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_MODER = Loc_Temp;
+	    /* These bits are written by software to configure the output type of the I/O port */
+	    Loc_Temp = GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_OTYPER;
+	    Loc_Temp &= ~(GPIO_OTYPER_MASK << (Add_pstrPinConfg->GPIO_Pin*PIN_OTYPER_OFFSET));
+		Loc_Temp |= (((Add_pstrPinConfg->GPIO_Mode>>PIN_MODER_OFFSET) & GPIO_OTYPER_MASK) << (Add_pstrPinConfg->GPIO_Pin*PIN_OTYPER_OFFSET));
+		GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_OTYPER = Loc_Temp;
+	    /* These bits are written by software to configure the I/O pull-up or pull-down */
+	    Loc_Temp = GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_PUPDR;
+	    Loc_Temp &= ~(GPIO_PUPDR_MASK << (Add_pstrPinConfg->GPIO_Pin*PIN_PUPDR_OFFSET));
+		Loc_Temp |= (((Add_pstrPinConfg->GPIO_Mode>>(PIN_MODER_OFFSET + PIN_OTYPER_OFFSET)) & GPIO_PUPDR_MASK) << (Add_pstrPinConfg->GPIO_Pin*PIN_PUPDR_OFFSET));
+		GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_PUPDR = Loc_Temp;
+		/* These bits are written by software to configure the I/O output speed */
+	    Loc_Temp = GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_OSPEEDR;
+	    Loc_Temp &= ~(GPIO_OSPEEDR_MASK << (Add_pstrPinConfg->GPIO_Pin*PIN_OSPEEDR_OFFSET));
+		Loc_Temp |= ((Add_pstrPinConfg->GPIO_Speed & GPIO_OSPEEDR_MASK) << (Add_pstrPinConfg->GPIO_Pin*PIN_OSPEEDR_OFFSET));
+		GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_OSPEEDR = Loc_Temp;
+		/* AF */
+		if(Add_pstrPinConfg->GPIO_Pin <= GPIO_PIN_7){
+			Loc_Temp = GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_AFRL;
+			Loc_Temp |= Add_pstrPinConfg->GPIO_ALTF << (PIN_ALTERNATIVE_OFFSET * Add_pstrPinConfg->GPIO_Pin);
+			GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_AFRL = Loc_Temp;
+		}
+		else{
+			Loc_Temp = GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_AFRH;
+			Loc_Temp |= Add_pstrPinConfg->GPIO_ALTF << (PIN_ALTERNATIVE_OFFSET * Add_pstrPinConfg->GPIO_Pin - GPIO_PIN_7);
+			GPIO_Ports[Add_pstrPinConfg->GPIO_Port]->GPIOx_AFRH = Loc_Temp;
+		}
+	}
+	return Loc_ErrorStatus;
+}
+
+ErrorStatus_t GPIO_SetPinValue(u32 Copy_GPIOPort, u32 Copy_GPIOPin, u32 Copy_GPIOPinState){
+	ErrorStatus_t Loc_ErrorStatus = GPIO_Ok;
+		if(Copy_GPIOPin > GPIO_PIN_15){
+			Loc_ErrorStatus = GPIO_WrongPin;
+		}
+		else if(Copy_GPIOPort > (u32)GPIOC){
+			Loc_ErrorStatus = GPIO_WrongPort;
+		}
+		else if(Copy_GPIOPinState != GPIO_LOW && Copy_GPIOPinState != GPIO_HIGH){
+				Loc_ErrorStatus = GPIO_WrongMode;
+		}
+		else{
+			GPIO_Ports[Copy_GPIOPort]->GPIOx_BSRR |= 1<<(Copy_GPIOPin+Copy_GPIOPinState);
+		}
+	return Loc_ErrorStatus;
+}
+
+ErrorStatus_t GPIO_GetPinValue(u32 Copy_GPIOPort, u32 Copy_GPIOPin, u32* Add_GPIOPinState){
+	ErrorStatus_t Loc_ErrorStatus = GPIO_Ok;
+		if(Copy_GPIOPin > GPIO_PIN_15){
+			Loc_ErrorStatus = GPIO_WrongPin;
+		}
+		else if(Copy_GPIOPort > (u32)GPIOC){
+			Loc_ErrorStatus = GPIO_WrongPort;
+		}
+		else if(Add_GPIOPinState == NULL){
+				Loc_ErrorStatus = GPIO_NULL;
+		}
+		else{
+			*Add_GPIOPinState = (GPIO_Ports[Copy_GPIOPort]->GPIOx_IDR>>Copy_GPIOPin) & 1;
+		}
+	return Loc_ErrorStatus;
+}
+
+
+
+
+
+
--- MCAL/GPIO/GPIO.h
+++ MCAL/GPIO/GPIO.h
@@ -8,9 +8,144 @@
 
 #ifndef GPIO_H_
 #define GPIO_H_
+/************************************************************************************
+ *                                       datatypes                                  *
+ * **********************************************************************************/
+typedef unsigned char         u8;
+typedef short unsigned int    u16;
+typedef unsigned int          u32;
+
+typedef enum
+{
+	GPIO_Ok,
+	GPIO_WrongPin,
+	GPIO_WrongPort,
+	GPIO_WrongMode,
+	GPIO_WrongSpeed,
+	GPIO_WrongAltf,
+	GPIO_NULL
+}ErrorStatus_t;
+
+typedef struct
+{
+	volatile u32 GPIOx_MODER;
+	volatile u32 GPIOx_OTYPER;
+	volatile u32 GPIOx_OSPEEDR;
+	volatile u32 GPIOx_PUPDR;
+	volatile u32 GPIOx_IDR;
+	volatile u32 GPIOx_ODR;
+	volatile u32 GPIOx_BSRR;
+	volatile u32 GPIOx_LCKR;
+	volatile u32 GPIOx_AFRL;
+	volatile u32 GPIOx_AFRH;
+
+}GPIO_Registers_t;
+
+typedef struct
+{
+	u32 GPIO_Port;
+	u32 GPIO_Mode;
+	u32 GPIO_Speed;
+	u32 GPIO_Pin;
+	u32 GPIO_ALTF;
+
+}GPIO_PinConfiguration_t;
+
+/************************************************************************************
+ *                                       #defines                                  *
+ * **********************************************************************************/
+
+#define GPIO_HIGH                   0
+#define GPIO_LOW                    16
+
+#define  GPIO_SPEED_LOW             0x00000000
+#define  GPIO_SPEED_MEDIUM          0x00000001
+#define  GPIO_SPEED_HIGH            0x00000002
+#define  GPIO_SPEED_VERYHIGH        0x00000003
+
+#define GPIO_AF00                   0x00000000
+#define GPIO_AF01                   0x00000001
+#define GPIO_AF02                   0x00000002
+#define GPIO_AF03                   0x00000003
+#define GPIO_AF04                   0x00000004
+#define GPIO_AF05                   0x00000005
+#define GPIO_AF06                   0x00000006
+#define GPIO_AF07                   0x00000007
+#define GPIO_AF08                   0x00000008
+#define GPIO_AF09                   0x00000009
+#define GPIO_AF10                   0x0000000A
+#define GPIO_AF11                   0x0000000B
+#define GPIO_AF12                   0x0000000C
+#define GPIO_AF13                   0x0000000D
+#define GPIO_AF14                   0x0000000E
+#define GPIO_AF15                   0x0000000F
+
+#define GPIO_MODE_INPUT_FL          0x00000000  /* moder 00*/
+#define GPIO_MODE_INPUT_PU          0x00000008  /* 4,5TH FOR PU OR PD */
+#define GPIO_MODE_INPUT_PD          0x00000010
+
+#define GPIO_MODE_OUTPUT_PP         0x00000001  /* moder 01*/
+#define GPIO_MODE_OUTPUT_PP_PU      0x00000009
+#define GPIO_MODE_OUTPUT_PP_PD      0x00000011
+#define GPIO_MODE_OUTPUT_OD         0x00000005  /* 3RD BIT FOR PP OR OD */
+#define GPIO_MODE_OUTPUT_OD_PU      0x0000000D
+#define GPIO_MODE_OUTPUT_OD_PD      0x00000015
+
+#define GPIO_MODE_AF_PP             0x00000002  /* moder 02*/
+#define GPIO_MODE_AF_PP_PU          0x0000000A
+#define GPIO_MODE_AF_PP_PD          0x00000012
+#define GPIO_MODE_AF_OD             0x00000006
+#define GPIO_MODE_AF_OD_PU          0x0000000E
+#define GPIO_MODE_AF_OD_PD          0x00000016
 
 
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
+#define GPIO_ALTERNATIVE_TIM1		0x01
+#define GPIO_ALTERNATIVE_TIM3		0x02
+#define GPIO_ALTERNATIVE_TIM9		0x03
+#define GPIO_ALTERNATIVE_I2C		0x04
+#define GPIO_ALTERNATIVE_SPI		0x05
+#define GPIO_ALTERNATIVE_SPI3		0x06
+#define GPIO_ALTERNATIVE_USART1		0x07
+#define GPIO_ALTERNATIVE_USART6		0x08
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
+#define GPIO_ALTERNATIVE_SYSTEM		0x00
 
 
+#define GPIOA	                    ((volatile GPIO_Registers_t* const)0x40020000)
+#define GPIOB	                    ((volatile GPIO_Registers_t* const)0x40020400)
+#define GPIOC	                    ((volatile GPIO_Registers_t* const)0x40020800)
+/*
+#define GPIOD	                    ((volatile GPIO_Registers_t* const)0x40020C00)
+#define GPIOE	                    ((volatile GPIO_Registers_t* const)0x40021000)
+#define GPIOH	                    ((volatile GPIO_Registers_t* const)0x40021C00)
+*/
+#define GPIO_PIN_0                  0x00000000
+#define GPIO_PIN_1                  0x00000001
+#define GPIO_PIN_2                  0x00000002
+#define GPIO_PIN_3                  0x00000003
+#define GPIO_PIN_4                  0x00000004
+#define GPIO_PIN_5                  0x00000005
+#define GPIO_PIN_6                  0x00000006
+#define GPIO_PIN_7                  0x00000007
+#define GPIO_PIN_8                  0x00000008
+#define GPIO_PIN_9                  0x00000009
+#define GPIO_PIN_10                 0x0000000A
+#define GPIO_PIN_11                 0x0000000B
+#define GPIO_PIN_12                 0x0000000C
+#define GPIO_PIN_13                 0x0000000D
+#define GPIO_PIN_14                 0x0000000E
+#define GPIO_PIN_15                 0x0000000F
 
+/************************************************************************************
+ *                                       functions                                  *
+ * **********************************************************************************/
+
+ErrorStatus_t GPIO_Init(GPIO_PinConfiguration_t* Add_pstrPinConfg);
+ErrorStatus_t GPIO_SetPinValue(u32 Copy_GPIOPort, u32 Copy_GPIOPin, u32 Copy_GPIOPinState);
+ErrorStatus_t GPIO_GetPinValue(u32 Copy_GPIOPort, u32 Copy_GPIOPin, u32* Add_GPIOPinState);
 #endif /* GPIO_H_ */
