252f16eac4f36a51813b8f5ed3214ba49060060b
Eman <eman.mo29100@gmail.com>
2024-02-16 19:53:34 +0200

    GPIO in progress

---
+++ MCAL/GPIO/GPIO.c
@@ -0,0 +1,6 @@
+/*
+ * GPIO.c
+ *
+ * Created: 2/15/2024 9:00:35 PM
+ *  Author: eman
+ */ 
---
+++ MCAL/GPIO/GPIO.h
@@ -0,0 +1,16 @@
+/*
+ * GPIO.h
+ *
+ * Created: 2/15/2024 8:59:58 PM
+ *  Author: eman
+ */ 
+
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+
+
+
+
+#endif /* GPIO_H_ */
